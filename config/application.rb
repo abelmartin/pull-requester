5e4d325e6662155c9c127a2739e7776309392d58
Abel Martin <abel.martin@gmail.com>
2013-08-17 19:18:11 -0400

    Heroku quick fix #2

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module PullRequester
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/lib)
 
+    # Heroku quick fix
+    config.assets.initialize_on_precompile = false
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,9 +17,6 @@ PullRequester::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
-  # Heroku quick fix
-  config.assets.initialize_on_precompile = false
-
   # Generate digests for assets URLs
   config.assets.digest = true
 
