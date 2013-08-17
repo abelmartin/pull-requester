37436c294867ca3d3e3f8ac75d5ba500d4bb0752
Abel Martin <abel.martin@gmail.com>
2013-08-17 02:28:06 -0400

    Reading Github credentials from yml OR ENV

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,11 @@
-github_credentials = YAML.load_file("#{Rails.root}/config/github_credentials.yml")
+if File.exists?("#{Rails.root}/config/github_credentials.yml")
+  github_credentials = YAML.load_file("#{Rails.root}/config/github_credentials.yml")
+else
+  github_credentials = {
+    'client_id' => ENV['GH_CLIENT_ID'],
+    'client_secret' => ENV['GH_CLIENT_SECRET']
+  }
+end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
