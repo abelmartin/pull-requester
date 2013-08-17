d8e3a73ad80f4ea710376ee01d3f4fec1d710b20
Abel Martin <abel.martin@gmail.com>
2013-08-17 00:43:50 -0400

    Adding the Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PullRequester::Application.config.secret_token = '9860df2a3016b331b2926ab702b0b49e52300c17e2b9f2ce823d9820daa1049d14576b72355a161c2f3d69344a1b917f8ac6c85c186f54895835d5028bd2be11'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+PullRequester::Application.routes.draw do
+  root :to => "home#index"
+end
