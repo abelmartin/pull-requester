57528645909744ae30af9e6f976cdfb4c7231a0d
Abel Martin <abel.martin@gmail.com>
2013-08-29 00:03:45 -0400

    Fixed issue where a user loses access to a repo he/she is watching
    * Fixes #27
    * Sneaking in zocial font to create new github button soon.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,13 @@ class HomeController < ApplicationController
       gh = Github.new(oauth_token: session[:gh_token])
       @watches = current_user.watches
       @watches.each do |watch|
-        watch.open_reqs = gh.pull_requests.all(watch.repo_owner, watch.repo_name)
+        begin
+          watch.open_reqs = gh.pull_requests.all(watch.repo_owner, watch.repo_name)
+        rescue
+          #Something went crazy when we tried to make this call.
+          #Consider real messages in the future for 401 vs 404 vs 500,etc.
+          watch.open_reqs = nil
+        end
       end
     end
   end
