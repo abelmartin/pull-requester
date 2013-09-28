eacdd763cb0cc64018770dd3e958c6a1c0a30627
Abel Martin <abel.martin@gmail.com>
2013-09-28 01:27:53 -0400

    Added placeholder pages for repository setting updates and account updates. * Not super happy about WHERE the repo edits would live right now.  Might need some models renaming, etc.

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,9 @@
+class AccountsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def show
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 PullRequester::Application.routes.draw do
   resources :watches
+  resource :account, only: [:show, :update]
 
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
   match '/ping' => 'home#ping'
+  get '/repositories' => 'watches#repositories', as: :repositories
+  put '/repositories' => 'watches#update_repositories', as: :update_repositories
 
   root to: "home#index"
 end
