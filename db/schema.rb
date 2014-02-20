e8cd3497fe49b71e302ffdc4e7239939c57819bd
Abel Martin <abel.martin@gmail.com>
2014-02-20 14:23:54 -0500

    Added index to repositories table.

--- db/schema.rb
+++ db/schema.rb
@@ -9,61 +9,63 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131227142207) do
+ActiveRecord::Schema.define(version: 20140220191846) do
 
-  create_table "build_services", :force => true do |t|
-    t.string "name",          :limit => 127, :null => false
-    t.string "badge_pattern", :limit => 256, :null => false
+  create_table "build_services", force: true do |t|
+    t.string "name",          limit: 127, null: false
+    t.string "badge_pattern", limit: 256, null: false
   end
 
-  create_table "pull_requests", :force => true do |t|
+  create_table "pull_requests", force: true do |t|
     t.integer  "repository_id"
-    t.integer  "comments",        :default => 0, :null => false
+    t.integer  "comments",        default: 0, null: false
     t.datetime "last_comment_at"
     t.string   "last_comment_by"
-    t.integer  "commits",         :default => 0, :null => false
+    t.integer  "commits",         default: 0, null: false
     t.datetime "last_commit_at"
-    t.integer  "additions",       :default => 0, :null => false
-    t.integer  "deletions",       :default => 0, :null => false
-    t.integer  "files_changed",   :default => 0, :null => false
-    t.datetime "created_at",                     :null => false
-    t.datetime "updated_at",                     :null => false
+    t.integer  "additions",       default: 0, null: false
+    t.integer  "deletions",       default: 0, null: false
+    t.integer  "files_changed",   default: 0, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table "repositories", :force => true do |t|
-    t.string   "name",             :null => false
-    t.integer  "gh_id",            :null => false
-    t.string   "owner",            :null => false
-    t.integer  "user_id",          :null => false
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+  create_table "repositories", force: true do |t|
+    t.string   "name",             null: false
+    t.integer  "gh_id",            null: false
+    t.string   "owner",            null: false
+    t.integer  "user_id",          null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
     t.integer  "build_service_id"
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "email",                                :default => "", :null => false
-    t.string   "encrypted_password",                   :default => "", :null => false
+  add_index "repositories", ["user_id"], name: "index_repositories_on_user_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email",                             default: "", null: false
+    t.string   "encrypted_password",                default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        :default => 0
+    t.integer  "sign_in_count",                     default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                           :null => false
-    t.datetime "updated_at",                                           :null => false
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
     t.string   "omni_uid"
-    t.string   "omni_provider",          :limit => 63
-    t.string   "name",                   :limit => 63
+    t.string   "omni_provider",          limit: 63
+    t.string   "name",                   limit: 63
     t.string   "avatar_url"
     t.string   "github_url"
-    t.string   "github_login",           :limit => 64
+    t.string   "github_login",           limit: 64
   end
 
-  add_index "users", ["email"], :name => "index_models_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_models_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_models_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
 
 end
