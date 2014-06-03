52b07e64e7f4aa9692d5b35b501631112d553fd7
Abel Martin <abel.martin@gmail.com>
2014-06-02 23:03:24 -0400

    Updated the configuration for github omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,13 +3,23 @@ if File.exists?("#{Rails.root}/config/github_credentials.yml")
 else
   github_credentials = {
     'client_id' => ENV['GH_CLIENT_ID'],
-    'client_secret' => ENV['GH_CLIENT_SECRET']
+    'client_secret' => ENV['GH_CLIENT_SECRET'],
+    'client_options' => ENV['GH_CLIENT_OPTIONS']
   }
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :github, github_credentials['client_id'], github_credentials['client_secret'], scope: 'user,repo'
+
+  gh_options = {scope: 'user,repo'}
+  if github_credentials['client_options']
+    gh_options.merge!(github_credentials['client_options'])
+  end
+
+  provider :github,
+    github_credentials['client_id'],
+    github_credentials['client_secret'],
+    gh_options
 end
 
 #OmniAuth.config.logger = Rails.logger
